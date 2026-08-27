d99133173b611dec915ed9c5a3e5d99b261ca983
song <>
2026-08-27 17:34:32 +0700

    增强入口改用懒加载属性转发，避免加载缺失原生模块并保留透明导入

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const Native = require('react-native/index.js');
+
+Object.defineProperties(module.exports, Object.getOwnPropertyDescriptors(Native));
+
+const lazy = (name, load) =>
+  Object.defineProperty(module.exports, name, {
+    configurable: true,
+    enumerable: true,
+    get() {
+      const value = load();
+      Object.defineProperty(module.exports, name, {
+        configurable: true,
+        enumerable: true,
+        value,
+      });
+      return value;
+    },
+  });
+
+lazy('FlashList', () => require('@song-react/flash-list').FlashList);
+lazy('Image', () => require('./components/Image').Image);
+lazy('Modal', () => require('./components/Modal').Modal);
+lazy('Pressable', () => require('./components/Pressable').Pressable);
+lazy('ScrollView', () => require('./components/ScrollView').ScrollView);
+lazy('Text', () => require('./components/Text').Text);
+lazy('TextInput', () => require('./components/TextInput').TextInput);
+lazy('View', () => require('./components/View').View);
+lazy('NativeTextInput', () => Native.TextInput);
+lazy('I18nProvider', () => require('./providers/I18nProvider').I18nProvider);
+lazy('useI18n', () => require('./providers/I18nProvider').useI18n);
+lazy('t', () => require('./providers/I18nProvider').t);
+lazy('QueryProvider', () => require('./providers/QueryProvider').QueryProvider);
+lazy('getQueryClient', () =>
+  require('./providers/QueryProvider').getQueryClient
+);
